9aa717a039ca9b7596c2346d4cd0d5757329b14d
AYUSH YADAV <ayushexx@gmail.com>
2022-05-22 11:49:53 +0530

    search-wc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+//import our Web Component
+import './web-components/search-result.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-web-components';
+  name: any;
 }
